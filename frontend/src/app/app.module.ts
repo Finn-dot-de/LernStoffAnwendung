d62a56f73ef009f8e13d17e13894933347f60650
LY04 <Finn.Laskowski@bamf.bund.de>
2024-05-13 09:45:13 +0200

    registrierung componete hinzugefügt für spätere Zwecke

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './Components/maincomponents/footer/footer.component';
 import { HeaderComponent } from './Components/maincomponents/header/header.component';
 import { LoginComponent } from './Components/Subcomponents/login/login.component';
+import { RegistrierungComponent } from './Components/Subcomponents/registrierung/registrierung.component';
 
 @NgModule({
     declarations: [
         AppComponent,
+        RegistrierungComponent,
     ],
     providers: [],
     bootstrap: [AppComponent],
