dacf1c1b01714c38bb837bb87aa64cc0c80df556
Finn <finnlaskowski761@gmail.com>
2024-06-30 00:09:54 +0200

    Registrierung wieder hinzugefügt aber leer

---
+++ frontend/src/app/Components/Subcomponents/registrierung/registrierung.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registrierung',
+  standalone: true,
+  imports: [],
+  templateUrl: './registrierung.component.html',
+  styleUrl: './registrierung.component.scss'
+})
+export class RegistrierungComponent {
+
+}
